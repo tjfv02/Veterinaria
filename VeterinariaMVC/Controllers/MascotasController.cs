03c00d2f85906dc050b8816270010be90a45b07c
Tito Fajardo <tjfv02@gmail.com>
2023-02-26 20:54:29 -0600

    CRUD Mascotas, Veterinarios y Veterinarias, Estilos

---
+++ VeterinariaMVC/Controllers/MascotasController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VeterinariaMVC;
+using VeterinariaMVC.Models;
+
+namespace VeterinariaMVC.Controllers
+{
+    public class MascotasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MascotasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Mascotas
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Mascota.Include(m => m.Usuario);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Mascotas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Mascota == null)
+            {
+                return NotFound();
+            }
+
+            var mascota = await _context.Mascota
+                .Include(m => m.Usuario)
+                .FirstOrDefaultAsync(m => m.MascotaId == id);
+            if (mascota == null)
+            {
+                return NotFound();
+            }
+
+            return View(mascota);
+        }
+
+        // GET: Mascotas/Create
+        public IActionResult Create()
+        {
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "Nombre");
+            return View();
+        }
+
+        // POST: Mascotas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MascotaId,Nombre,Edad,Peso,UsuarioId")] Mascota mascota)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(mascota);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "Nombre", mascota.UsuarioId);
+            return View(mascota);
+        }
+
+        // GET: Mascotas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Mascota == null)
+            {
+                return NotFound();
+            }
+
+            var mascota = await _context.Mascota.FindAsync(id);
+            if (mascota == null)
+            {
+                return NotFound();
+            }
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "UsuarioId", mascota.UsuarioId);
+            return View(mascota);
+        }
+
+        // POST: Mascotas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MascotaId,Nombre,Edad,Peso,UsuarioId")] Mascota mascota)
+        {
+            if (id != mascota.MascotaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mascota);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MascotaExists(mascota.MascotaId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "UsuarioId", mascota.UsuarioId);
+            return View(mascota);
+        }
+
+        // GET: Mascotas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Mascota == null)
+            {
+                return NotFound();
+            }
+
+            var mascota = await _context.Mascota
+                .Include(m => m.Usuario)
+                .FirstOrDefaultAsync(m => m.MascotaId == id);
+            if (mascota == null)
+            {
+                return NotFound();
+            }
+
+            return View(mascota);
+        }
+
+        // POST: Mascotas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Mascota == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Mascota'  is null.");
+            }
+            var mascota = await _context.Mascota.FindAsync(id);
+            if (mascota != null)
+            {
+                _context.Mascota.Remove(mascota);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MascotaExists(int id)
+        {
+          return (_context.Mascota?.Any(e => e.MascotaId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ VeterinariaMVC/Controllers/VeterinariosController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VeterinariaMVC;
+using VeterinariaMVC.Models;
+
+namespace VeterinariaMVC.Controllers
+{
+    public class VeterinariosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VeterinariosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Veterinarios
+        public async Task<IActionResult> Index()
+        {
+              return _context.Veterinarios != null ? 
+                          View(await _context.Veterinarios.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Veterinarios'  is null.");
+        }
+
+        // GET: Veterinarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Veterinarios == null)
+            {
+                return NotFound();
+            }
+
+            var veterinario = await _context.Veterinarios
+                .FirstOrDefaultAsync(m => m.VeterinarioId == id);
+            if (veterinario == null)
+            {
+                return NotFound();
+            }
+
+            return View(veterinario);
+        }
+
+        // GET: Veterinarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Veterinarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("VeterinarioId,Nombre,Apellido,VeterinariaId")] Veterinario veterinario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(veterinario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(veterinario);
+        }
+
+        // GET: Veterinarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Veterinarios == null)
+            {
+                return NotFound();
+            }
+
+            var veterinario = await _context.Veterinarios.FindAsync(id);
+            if (veterinario == null)
+            {
+                return NotFound();
+            }
+            return View(veterinario);
+        }
+
+        // POST: Veterinarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("VeterinarioId,Nombre,Apellido,VeterinariaId")] Veterinario veterinario)
+        {
+            if (id != veterinario.VeterinarioId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(veterinario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VeterinarioExists(veterinario.VeterinarioId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(veterinario);
+        }
+
+        // GET: Veterinarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Veterinarios == null)
+            {
+                return NotFound();
+            }
+
+            var veterinario = await _context.Veterinarios
+                .FirstOrDefaultAsync(m => m.VeterinarioId == id);
+            if (veterinario == null)
+            {
+                return NotFound();
+            }
+
+            return View(veterinario);
+        }
+
+        // POST: Veterinarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Veterinarios == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Veterinarios'  is null.");
+            }
+            var veterinario = await _context.Veterinarios.FindAsync(id);
+            if (veterinario != null)
+            {
+                _context.Veterinarios.Remove(veterinario);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VeterinarioExists(int id)
+        {
+          return (_context.Veterinarios?.Any(e => e.VeterinarioId == id)).GetValueOrDefault();
+        }
+    }
+}
