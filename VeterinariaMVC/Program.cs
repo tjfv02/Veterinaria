807f1d6d6d5edbf97fa0c7f9c07d52bd7c138bfa
Tito Fajardo <tjfv02@gmail.com>
2023-04-26 13:21:59 -0600

    Cambio de Layout para Login

--- VeterinariaMVC/Program.cs
+++ VeterinariaMVC/Program.cs
@@ -39,6 +39,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=SignIn}");
 
 app.Run();
